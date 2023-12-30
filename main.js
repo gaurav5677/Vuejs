d4546d6a101dec3d65d12f40c91e3bae59b99403
Gaurav <gauravpatilfamily7@gmail.com>
2023-12-30 15:52:41 +0530

    binding Attribute

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      description: "this is high quality warm Socks",
+      image: "./assets/images/socks_green.jpg",
+      link: "https://www.vuemastery.com/?fpr=victor71&gclid=CjwKCAiAnL-sBhBnEiwAJRGigpMSJNbn07a1huoqqnnx7-crIr03BU6cMIZHpyDiyZZHKGbL1OJ6HhoC66cQAvD_BwE",
     };
   },
 });
